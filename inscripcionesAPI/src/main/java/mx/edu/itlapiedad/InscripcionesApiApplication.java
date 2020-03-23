59f5e68d0c0544934c85e8253c755a08b7651736
maxxor704 <miguelagushreyes@gmail.com>
2020-03-22 23:18:30 -0600

    Add files via upload

---
+++ inscripcionesAPI/src/main/java/mx/edu/itlapiedad/InscripcionesApiApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.itlapiedad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscripcionesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscripcionesApiApplication.class, args);
+	}
+
+}
