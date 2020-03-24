d748f4b79fd4a5381abf3f4276ba6b6f45176217
Doroty <Doroty@192.168.3.9>
2020-03-23 23:39:13 -0600

    otra vez

--- inscripcionesAPI/src/main/java/mx/edu/itlapiedad/InscripcionesApiApplication.java
+++ inscripcionesAPI/src/main/java/mx/edu/itlapiedad/InscripcionesApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InscripcionesApiApplication {
-
+//comentari
 	public static void main(String[] args) {
 		SpringApplication.run(InscripcionesApiApplication.class, args);
 	}
