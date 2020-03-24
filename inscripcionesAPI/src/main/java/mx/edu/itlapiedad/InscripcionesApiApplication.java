18fa85a86972308c46efeb2d706f5426ac62a85b
luisa <luisa@DESKTOP-4QGDKTS>
2020-03-23 23:36:09 -0600

    Comentario pro

--- inscripcionesAPI/src/main/java/mx/edu/itlapiedad/InscripcionesApiApplication.java
+++ inscripcionesAPI/src/main/java/mx/edu/itlapiedad/InscripcionesApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InscripcionesApiApplication {
 
+	//Comentario de govea para probar que jale
 	public static void main(String[] args) {
 		SpringApplication.run(InscripcionesApiApplication.class, args);
 	}
