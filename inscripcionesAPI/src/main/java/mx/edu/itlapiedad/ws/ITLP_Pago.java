8b1b52040e992f1de3f45d314c32c072965bdc3d
Daniel <daniel326b7@gmail.com>
2020-05-20 23:39:38 -0500

    necesitamos un psicologo

---
+++ inscripcionesAPI/src/main/java/mx/edu/itlapiedad/ws/ITLP_Pago.java
@@ -0,0 +1,32 @@
+package mx.edu.itlapiedad.ws;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import mx.edu.itlapiedad.DAO.DAO;
+import mx.edu.itlapiedad.models.InsertarCargaAcademica;
+import mx.edu.itlapiedad.models.Pago;
+
+@RestController
+@RequestMapping("/tec")
+@CrossOrigin(origins = "*", methods={RequestMethod.GET})
+public class ITLP_Pago {
+	@Autowired
+	DAO repositorio;
+	
+	//RUTA DE GUARDAR PAGO
+		@PostMapping("guardarPago")
+		public void guardarpago(@RequestBody Pago pago) {
+			repositorio.guardarpago(pago);
+		}
+
+}
