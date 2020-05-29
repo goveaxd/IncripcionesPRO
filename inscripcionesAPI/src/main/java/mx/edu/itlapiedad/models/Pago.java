579eed6f79bc452ab4cbb5aa0459ed557da1c318
Govii <49994798+goveaxd@users.noreply.github.com>
2020-05-28 19:50:27 -0500

    De nuevo

--- inscripcionesAPI/src/main/java/mx/edu/itlapiedad/models/Pago.java
+++ inscripcionesAPI/src/main/java/mx/edu/itlapiedad/models/Pago.java
@@ -5,16 +5,21 @@ public class Pago {
 	String estado;
 	int semestre_idsemestre;
 	int alumnos_idAlumno;
+	String ticket;
+	int noTransaccion;
 	public Pago() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Pago(int idpago, String estado, int semestre_idsemestre, int alumnos_idAlumno) {
+	public Pago(int idpago, String estado, int semestre_idsemestre, int alumnos_idAlumno, String ticket,
+			int noTransaccion) {
 		super();
 		this.idpago = idpago;
 		this.estado = estado;
 		this.semestre_idsemestre = semestre_idsemestre;
 		this.alumnos_idAlumno = alumnos_idAlumno;
+		this.ticket = ticket;
+		this.noTransaccion = noTransaccion;
 	}
 	public int getIdpago() {
 		return idpago;
@@ -40,6 +45,17 @@ public class Pago {
 	public void setAlumnos_idAlumno(int alumnos_idAlumno) {
 		this.alumnos_idAlumno = alumnos_idAlumno;
 	}
-	
-	
+	public String getTicket() {
+		return ticket;
+	}
+	public void setTicket(String ticket) {
+		this.ticket = ticket;
+	}
+	public int getNoTransaccion() {
+		return noTransaccion;
+	}
+	public void setNoTransaccion(int noTransaccion) {
+		this.noTransaccion = noTransaccion;
+	}
+		
 }
